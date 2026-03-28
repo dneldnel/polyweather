f8e34e21c143bd4fcb35358d5d129a3b9ba07e83
dneldnel <dneldnel@users.noreply.github.com>
2026-03-27 23:04:55 -0400

    Remove Next.js leftovers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: [".next/**", "dist/**", "node_modules/**"],
+    ignores: ["dist/**", "node_modules/**"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
